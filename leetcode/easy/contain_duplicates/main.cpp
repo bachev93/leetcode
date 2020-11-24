47e18f2954c4d2b73b8dea3e1925c92301c57c58
Sergey <sbachev@aorti.ru>
2020-11-24 10:19:17 +0300

    contain_duplicates solution was added

---
+++ leetcode/easy/contain_duplicates/main.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <ios>
+#include <iostream>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+bool contains_duplicate(vector<int>& nums) {
+  sort(nums.begin(), nums.end());
+  const auto it = unique(nums.begin(), nums.end());
+  return (it == nums.end() ? false : true);
+}
+
+// another approach
+bool contains_duplicate2(vector<int>& nums) {
+  set<int> set_nums(nums.begin(), nums.end());
+  return nums.size() > set_nums.size();
+}
+
+// simple approach
+bool contains_duplicate3(vector<int>& nums) {
+  for (auto i = 0u; i < nums.size(); ++i) {
+    for (auto j = 0u; j < i; ++j) {
+      if (nums[j] == nums[i]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+int main() {
+  vector<int> nums = {1, 2, 3, 1};
+  cout << boolalpha << contains_duplicate2(nums) << endl;
+  //  cout << boolalpha << contains_duplicate(nums) << endl;
+  //  cout << boolalpha << contains_duplicate3(nums) << endl;
+
+  return 0;
+}
--- leetcode/easy/easy.pro
+++ leetcode/easy/easy.pro
@@ -4,4 +4,5 @@ SUBDIRS += \
     remove_duplicates \
     two_sum \
     reverse_integer \
-    roman_to_int
+    roman_to_int \
+    contain_duplicates
