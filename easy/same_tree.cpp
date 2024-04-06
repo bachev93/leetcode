d5a11b1c63fa55824097ee266521f8a03c53c5e2
Sergey Bachev <bachev93@mail.ru>
2024-04-06 22:35:04 +0300

    add same tree task solution

--- easy/same_tree.cpp
+++ easy/same_tree.cpp
@@ -2,8 +2,40 @@
 
 using namespace std;
 
+// Definition for a binary tree node.
+  struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+
+// Base solution
+bool isSameTree(TreeNode* p, TreeNode* q) {
+  if(!p && !q) {
+      return true;
+  }
+
+  if(p && q && p->val == q->val) {
+      return isSameTree(p->left, q->left) && isSameTree(p->right, q->right); 
+  }
+
+  return false;
+}
+
+// some improvement in base solution
+bool isSameTree2(TreeNode* p, TreeNode* q) {
+  if(!p || !q) {
+      return p == q;
+  }
+
+  return p->val == q->val && isSameTree2(p->left, q->left) && isSameTree2(p->right, q->right);
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+  cout << "same tree: " << isSameTree(nullptr, new TreeNode(10)) << endl;
+  return 0;
 }
