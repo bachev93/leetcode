6f3ffc1e41fedee1f8e4241f50dcd8c1782f25de
Sergey Bachev <bachev93@mail.ru>
2024-04-22 00:00:56 +0300

    add container_must_water solution

---
+++ medium/container_most_water.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int maxArea(vector<int>& height) {
+  int max_area{0};
+  const int height_len = height.size();
+  int left = 0;
+  int right = height_len - 1;
+
+  while(left < right) {
+    max_area = max(max_area, min(height[left], height[right]) * (right - left));
+
+    if(height[left] < height[right]) {
+      ++left;
+    } else {
+      --right;
+    }
+  }
+
+  return max_area;
+}
+
+int main() {
+  cout << "container with must water: " << endl;
+
+  vector<int> height{1,8,6,2,5,4,8,3,7};
+  vector<int> height2{1, 1};
+
+  cout << "result = " << maxArea(height) << endl;
+
+  return 0;
+}
